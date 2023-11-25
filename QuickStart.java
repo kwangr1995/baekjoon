160240a215e43f4aea84a4da067b7765e277a346
kwangr1995 <kwangr1995@gmail.com>
2023-11-25 17:29:43 +0900

    Update QuickStart.java

--- QuickStart.java
+++ QuickStart.java
@@ -1,6 +1,14 @@
 public class QuickStart {
     public static void main(String[] args) {
-        System.out.println("Hi there");
-        
+        String x = "1234";
+        System.out.println(x);
+
+        int abcd = 1; //abcd를 정수형으로 설정, 1할당
+        String efgh  = "faker"; //efgh를 문자형으로 설정, faker 할당
+
+        System.out.println(abcd + abcd); //abcd에 할당된 값 두 번 더하기
+        System.out.println(efgh); //efgh 값 출력
     }
 }
+
+
