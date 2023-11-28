afdc1f460c0f55bd4b40f253b03b6afd9d57e6e9
kwangr1995 <kwangr1995@gmail.com>
2023-11-28 15:03:55 +0900

    1128

--- project_test/hello.java
+++
@@ -1,8 +0,0 @@
-package project_test;
-public class hello {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world_JAVA");
-    }
-}
-
---
+++ test/hellotest2.java
@@ -0,0 +1,27 @@
+package test;
+
+public class hellotest2 {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("hello_a"); //println함수, 개행출력
+        System.out.println("hello_b"); //println함수, 개행출력
+        System.out.print("hello_2");//print함수, 개행없이 출력
+        System.out.println("hello_3");//print함수, 개행없이 출력
+        System.out.println("hello_4");//print함수, 개행없이 출력
+        System.out.println();//줄바꿈
+        System.out.println(); //sys치고 ctrl+space
+        System.out.println("1      2");
+        System.out.println("3      4");
+        System.out.println("=================================");
+        System.out.print("1 ");
+        System.out.println("2");
+        System.out.print("3 ");
+        System.out.println("4");
+        System.out.print("1 ");
+        System.out.print("2 ");
+        System.out.println();
+        System.out.print("3 ");
+        System.out.print("4");
+    }
+    
+}
