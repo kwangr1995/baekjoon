36cfb12eef37f14f4895fd8c5951b1b4c4d975e2
kwangr1995 <kwangr1995@gmail.com>
2023-12-15 18:03:28 +0900

    1215

--- backjoon/02_ConditionalStatement/src/_07_2480.java
+++ backjoon/02_ConditionalStatement/src/_07_2480.java
@@ -9,8 +9,31 @@ public class _07_2480 {
         int b = scan.nextInt();
         int c = scan.nextInt();
 
-        if(a==b&&b==c)System.out.println();
+        int case_num = 3;
 
+        if(a==b&&b==c) case_num = 1;
+        if(a==b&&a!=c) case_num = 2;
+        if(a!=b&&a==c) case_num = 2;
+        if(b==c&&a!=b) case_num = 2;
 
+        int same_2 = a;
+
+        int max = 0;
+
+        if(case_num == 1)System.out.println(10000+(a*1000));
+
+        if(case_num == 2){
+            if(a==b&&a!=c) same_2 = a;
+            if(a!=b&&a==c) same_2 = a;
+            if(b==c&&a!=b) same_2 = b;
+            System.out.println(1000+(same_2)*100);
+        }
+
+        if(case_num == 3){
+            if(a>b&&a>c) max = a;
+            if(b>a&&b>c) max = b;
+            if(c>a&&c>b) max = c;
+            System.out.println(max*100);
+        }
     }
 }
