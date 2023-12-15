8a4ff9367ccec3245e04085ada0edbe73eb2eb59
kwangr1995 <kwangr1995@gmail.com>
2023-12-15 13:59:04 +0900

    1215

--- backjoon/02_ConditionalStatement/src/_07_2480.java
+++ backjoon/02_ConditionalStatement/src/_07_2480.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class _07_2480 {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        if(a==b&&b==c)System.out.println();
+
+
+    }
 }
